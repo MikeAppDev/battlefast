a4f3841da083ed579c3b5bdef95342b1eac955e5
Michael <michael.sarrazin@webedia-group.com>
2022-05-23 14:27:58 +0200

    first commit

---
+++ html/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Titre de la page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/dist/assets/img/favicon.ico" />
+    <link rel="stylesheet" href="/dist/assets/styles/main.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+</head>
+
+<body class="home">
+    <header id="header" class="header">
+        <h1>Je suis le header</h1>
+    </header>
+
+    <main>
+        <div class="container">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, molestiae inventore perspiciatis consequuntur officiis odit praesentium harum maiores ab, sed tempore esse ex nam, et reiciendis modi odio dolorem? Veritatis.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, molestiae inventore perspiciatis consequuntur officiis odit praesentium harum maiores ab, sed tempore esse ex nam, et reiciendis modi odio dolorem? Veritatis.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, molestiae inventore perspiciatis consequuntur officiis odit praesentium harum maiores ab, sed tempore esse ex nam, et reiciendis modi odio dolorem? Veritatis.</p>
+			<img src="dist/assets/img/bg.jpg" alt="img test" style="width: 100%; display: block">
+		</div>
+    </main>
+
+    <!-- modal player -->
+    <footer id="footer" class="footer">
+        <div class="container">
+            Je suis le footer
+        </div>
+    </footer>
+
+    <script src="/dist/assets/js/bundle.js"></script>
+</body>
+</html>
